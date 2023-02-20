2f6e0a4aa6971843ab3c0541a24522b08e1c8a67
TorYiMeng <toryimeng9@gmail.com>
2023-02-20 14:16:33 +0700

    Project update

---
+++ front-end/src/components/Filters.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { Form, Col, Row, Button } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { filterPizza } from "../actions/pizzaAction";
+const Filters = () => {
+  const [searchkey, setsearchkey] = useState("");
+  const [category, setcategory] = useState("all");
+  const dispatch = useDispatch();
+  return (
+    <div className=" mt-2">
+      <Form>
+        <Row>
+          <Col>
+            <Form.Control
+              value={searchkey}
+              onChange={(e) => setsearchkey(e.target.value)}
+              placeholder="serach pizza"
+            />
+          </Col>
+          <Col>
+            <select
+              className="form-select"
+              value={category}
+              onChange={(e) => setcategory(e.target.value)}
+            >
+              <option>All</option>
+              <option>pizza</option>
+              <option>beverage</option>
+            </select>
+          </Col>
+          <Col>
+            <Button
+              type="button"
+              className="btn btn-danger"
+              onClick={() => {
+                dispatch(filterPizza(searchkey, category));
+              }}
+            >
+              Search
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+    </div>
+  );
+};
+
+export default Filters;
